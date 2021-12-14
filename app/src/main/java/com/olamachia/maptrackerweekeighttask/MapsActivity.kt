b55a94f82675f8fd2fa25318603b9fe236b56052
Decagon <mac@macs-MacBook-Air.local>
2021-12-14 19:44:08 +0100

    location

--- app/src/main/java/com/olamachia/maptrackerweekeighttask/MapsActivity.kt
+++ app/src/main/java/com/olamachia/maptrackerweekeighttask/MapsActivity.kt
@@ -1,24 +1,36 @@
 package com.olamachia.maptrackerweekeighttask
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.olamachia.maptrackerweekeighttask.databinding.ActivityMainBinding
+import com.google.android.gms.maps.model.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.olamachia.maptrackerweekeighttask.Model.location
+import com.olamachia.maptrackerweekeighttask.databinding.ActivityMaps2Binding
 import com.olamachia.maptrackerweekeighttask.databinding.ActivityMapsBinding
-import java.util.*
+import androidx.core.location.LocationManagerCompat.getCurrentLocation as getCurrentLocation1
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var fusedLocClient: FusedLocationProviderClient
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+
+    // use it to request location updates and get the latest location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,56 +42,96 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    }
+    // use it to request location updates and get the latest location
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        val latitude = 6.47420
-        val longitude = 3.63031
-
-        val zoomLevel = 15f
-        //creating a latlng object called home
-        val homeLatLng = LatLng(latitude, longitude)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-        map.addMarker(MarkerOptions().position(homeLatLng))
-        setMapLongClick(map)
-        setPoiClick(map)
+        getCurrentLocation()
+        setupLocClient()
 
     }
+    private fun setupLocClient() {
+        fusedLocClient =
+            LocationServices.getFusedLocationProviderClient(this)
+    }
 
-    private fun setMapLongClick(map: GoogleMap) {
-        map.setOnMapLongClickListener { latLng ->
-            // snippet is additional text that displays below the title
-            val snippet = String.format(
-                Locale.getDefault(),
-                "Lat: %1$.5f, Long: %2$.5f",
-                latLng.latitude,
-                latLng.longitude
-            )
-            map.addMarker(
-                MarkerOptions()
-                    .position(latLng)
-            )
+    // prompt the user to grant/deny access
+    private fun requestLocPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), //permission in the manifest
+            REQUEST_LOCATION)
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1 //request code to identify specific permission request
+        private const val TAG = "MapsActivity" // for debugging
+
+    }
+    private fun getCurrentLocation() {
+        // Check if the ACCESS_FINE_LOCATION permission was granted before requesting a location
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // call requestLocPermissions() if permission isn't granted
+            requestLocPermissions()
+        } else {
+
+            fusedLocClient.lastLocation.addOnCompleteListener {
+                // lastLocation is a task running in the background
+                val location = it.result //obtain location
+                //reference to the database
+                val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                val ref: DatabaseReference = database.getReference("Ahmed")
+                if (location != null) {
+
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    // create a marker at the exact location
+                    map.addMarker(
+                        MarkerOptions().position(latLng)
+                            .title("You are currently here!")
+                    )
+                        ?.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.lanre))
+                    // create an object that will specify how the camera will be updated
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+
+                    map.moveCamera(update)
+                    //Save the location data to the database
+
+                    ref.child(
+                        "Ahmed"
+                    ).setValue(location("Ahmed", location.latitude, location.longitude))
+                } else {
+                    // if location is null , log an error message
+                    Log.e(TAG, "No location found")
+                }
+
+
+            }
         }
     }
-
-    private fun setPoiClick(map: GoogleMap) {
-        map.setOnPoiClickListener { poi ->
-            val poiMarker = map.addMarker(
-                MarkerOptions()
-                    .position(poi.latLng)
-                    .title(poi.name)
-            )
-            poiMarker?.showInfoWindow()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //check if the request code matches the REQUEST_LOCATION
+        if (requestCode == REQUEST_LOCATION)
+        {
+            //check if grantResults contains PERMISSION_GRANTED.If it does, call getCurrentLocation()
+            if (grantResults.size == 1 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                //if it doesn`t log an error message
+                Log.e(TAG, "Location permission has been denied")
+            }
         }
     }
 }
+
